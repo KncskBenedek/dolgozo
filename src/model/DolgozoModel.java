e89b5ea4d829570ac1d58bb937b9a1c13ff4a55b
Koncsik Benedek <koncsik.benedek@dk.akkszalezi.hu>
2023-02-27 09:42:26 +0100

    alap DolgozoModel

---
+++ src/model/DolgozoModel.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author koncsik.benedek
+ */
+public class DolgozoModel {
+    private String nev;
+    private int kor;
+    private char neme;
+    private int munkToltEv;
+
+    public DolgozoModel(String nev, int kor, char neme, int munkToltEv) {
+        this.nev = nev;
+        this.kor = kor;
+        this.neme = neme;
+        this.munkToltEv = munkToltEv;
+    }
+
+    public DolgozoModel(String nev, int kor, char neme) {
+        this(nev, kor, neme, 0);
+    }
+
+    public int getKor() {
+        return kor;
+    }
+
+    public int getMunkToltEv() {
+        return munkToltEv;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public char getNeme() {
+        return neme;
+    }
+}
